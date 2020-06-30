b5e320c5cbd7ae27d3e52da3183571839a318cf5
shaochuankai <shaochuankai@163.com>
2020-06-30 15:49:20 +0800

    添加测试代码

--- app/src/main/java/com/sck/guide/MainActivity.kt
+++ app/src/main/java/com/sck/guide/MainActivity.kt
@@ -2,10 +2,22 @@ package com.sck.guide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        GuideManager.init(this, true)
+        tv.setOnClickListener {
+            GuideManager.begin()
+                .add(GuideParams(tv, R.layout.guide_demo_bottom,"a"))
+                .add(GuideParams(tv1, R.layout.guide_demo_bottom, "b",gravity = Gravity.LEFT))
+                .show(this)
+        }
     }
 }
